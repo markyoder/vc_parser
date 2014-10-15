ba118f140f4218c00f1493b1590ce20fb1073c09
MarkYoder <mark.yoder@gmail.com>
2014-10-15 13:52:05 -0700

    adding geodetic bits...

--- vc_geodetic.py
+++ vc_geodetic.py
@@ -58,6 +58,131 @@ import multiprocessing as mpp
 #
 #import ANSStools
 #import BASScast
+#
+napa_region_section_filter = {'filter':set([45, 50, 172, 171, 170, 169, 44, 168, 167, 139, 40, 142, 41, 46])}
+
+emc_section_filter = {'filter': (16, 17, 18, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 56, 57, 69, 70, 73, 83, 84, 92, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 123, 124, 125, 126, 149)}
+allcal_full_mks = '../ALLCAL2_1-7-11_no-creep_dyn-05_st-20.h5'
+default_sim_file=allcal_full_mks
+#
+emc_sections = emc_section_filter['filter']
+napa_sections = napa_region_section_filter['filter']
+#
+def blocks_test(n_cycles=1):
+	#
+	print "doing vanilla version:"
+	total_time=0.0
+	for i in xrange(n_cycles):
+		t0=time.time()
+		B=get_blocks_dict()
+		t1=time.time()-t0
+		print "delta_t: %f" % t1
+		total_time+=t1
+	print "mean time: %f" % (total_time/float(n_cycles))
+	#
+	total_time=0.
+	print "doing Pool.apply_async() version"
+	for i in xrange(n_cycles):
+		t0=time.time()
+		B=get_blocks_dict_pool()
+		t1=time.time()-t0
+		print "delta_t: %f" % t1
+		total_time+=t1
+	print "mean time: %f" % (total_time/float(n_cycles))
+	#
+	#
+	total_time=0.
+	print "doing Process() version"
+	for i in xrange(n_cycles):
+		t0=time.time()
+		B=get_blocks_dict_process()
+		t1=time.time()-t0
+		print "delta_t: %f" % t1
+		total_time+=t1
+	print "mean time: %f" % (total_time/float(n_cycles))
+	#
+	print "finished."
+#
+#
+def get_blocks_dict_process(sim_file=default_sim_file, faults=None, sections=None, n_cpus=None):
+	# 
+	if n_cpus==None: n_cpus = mpp.cpu_count()
+	#
+	
+	# pool with 1 cpu is slower by about a factor of 1.5
+	pipe_func, pipe_proc = mpp.Pipe()
+	process = mpp.Process(target = get_blocks_dict, kwargs={'sim_file':sim_file, 'faults':faults, 'sections':sections, 'pipe':pipe_func})
+	
+	process.start()
+	#process.join()
+	#
+	return pipe_proc.recv()
+#
+def get_blocks_dict_pool(sim_file=default_sim_file, faults=None, sections=None, n_cpus=None):
+	# 
+	if n_cpus==None: n_cpus = mpp.cpu_count()
+	#
+	
+	# pool with 1 cpu is slower by about a factor of 1.5
+	pool = mpp.Pool(n_cpus)
+	X = []
+	j=0
+	
+	X += [pool.apply_async(func=get_blocks_dict, kwds={'sim_file':sim_file, 'faults':faults, 'sections':sections})]
+	pool.close()
+	#
+	#return pool.get()
+	if len(X)==1: return X[0].get()
+	if len(X)>1:
+		# reduce() syndax?
+		r_dict = X[0].get()
+		for x in X[1:]:
+			r_dict.update(x.get())
+		#
+		return r_dict
+		
+def get_blocks_dict(sim_file=default_sim_file, faults=None, sections=None, pipe=None):
+	# return a dictionary of block dict-like objects (they may be recArrays.
+	# this step can likely be skipped, since block_info_table is typically written
+	# in block_id order, and so already indexed. this, however, is not necessarilly
+	# enforced.
+	#
+	
+	#block_dict = {}		#--> {block_id:{values...}}
+	#
+	with h5py.File(sim_file, 'r') as vc_data:
+		block_info = vc_data['block_info_table']
+		'''
+		for rw in block_info:
+			#block_dict[rw['block_id']] = rw		# if we want this to actually be a dict, it might be necessary to 
+												# loop over vc_data['info_table'].dtype.names
+			block_dict[rw['block_id']] = {name:rw[name] for name in block_info.dtype.names}
+		'''
+		block_dict = {rw['block_id']:{name:rw[name] for name in block_info.dtype.names} for rw in block_info if (sections==None or rw['section_id'] in sections) or (faults==None or rw['fault_id'] in faults)}
+		#
+	#
+	if pipe!=None:
+		pipe.send(block_dict)
+	else:
+		return block_dict
+#
+def blockwise_slip(sim_file=default_sim_file, faults=None, sections=None, pipe=None):
+	block_info = get_blocks_dict(sim_file=sim_file, faults=faults, sections=sections)
+	#
+	# add mean position to block_info:
+	for key in block_info.keys():
+		rw=block_info[key]
+		mean_x = numpy.mean([rw['m_x_pt%d' % j] for j in [1,2,3,4]])
+		mean_y = numpy.mean([rw['m_y_pt%d' % j] for j in [1,2,3,4]])
+		mean_z = numpy.mean([rw['m_z_pt%d' % j] for j in [1,2,3,4]])
+		#
+		block_info[key].update({'mean_x':mean_x, 'mean_y':mean_y, 'mean_z':mean_z})
+	#
+	# so now, we can spin through the event_sweep and generate positional vectors. how do we get directon?
+	# use neighboring elements? this will be a bit tricky since elements may be stacked on top of one another.
+	# note, however, fault/section/block do contain rake/strike, etc. info...
+	#
+	return block_info
 
 
 

847a3b1b9ef92f45b734cacf224c75659e6e11bb
MarkYoder <mark.yoder@gmail.com>
2014-10-25 23:35:22 -0700

    adding rotation matrices

--- vc_geodetic.py
+++ vc_geodetic.py
@@ -524,4 +524,25 @@ def plot_initial_section_positions(blockwise_obj='dumps/blockwise_slip.pkl', sec
 	#
 	return blockwise_obj
 #
+def rotation_matrix_general(axis=None, theta=None):
+	# generalize rotation matrix from:
+	# http://stackoverflow.com/questions/6802577/python-rotation-of-3d-vector
+	#
+	"""
+	#Return the rotation matrix associated with counterclockwise rotation about
+	#the given axis by theta radians.
+    """
+	axis = numpy.asarray(axis)
+	theta = numpy.asarray(theta)
+	axis = axis/math.sqrt(numpy.dot(axis, axis))
+	a = math.cos(theta/2.)
+	b, c, d = -axis*math.sin(theta/2.)
+	aa, bb, cc, dd = a*a, b*b, c*c, d*d
+	bc, ad, ac, ab, bd, cd = b*c, a*d, a*c, a*b, b*d, c*d
+	
+	return numpy.array([[aa+bb-cc-dd, 2.*(bc+ad), 2.*(bd-ac)],
+			[2.*(bc-ad), aa+cc-bb-dd, 2.*(cd+ab)],
+			[2.*(bd+ac), 2.*(cd-ab), aa+dd-bb-cc]])
 
+def rotate_vector_general(vector=None, axis=None, theta=None):
+	return numpy.dot(rotation_matrix_general(axis, theta), vector)

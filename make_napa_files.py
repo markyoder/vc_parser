174842a0e216cb37182f8b827429800012d89683
mark yoder <mark.yoder@gmail.com>
2014-12-08 22:44:42 -0800

    synching...

--- make_napa_files.py
+++ make_napa_files.py
@@ -15,7 +15,8 @@ for this_m in [6.0, 7.0]:
 
 	b = vc_parser.recurrence_figs(section_ids=napa_sections, m0=this_m, output_dir=weibull_recurr_output_dir)
 	
-	b= vc_parser.EMC_EWT_figs(section_ids=napa_sections, m0=this_m, fits_data_file_CDF='%s/VC_CDF_Weibull_fits_dump.npy' % weibull_recurr_output_dir$, WT_catalog_format='data/VC_CFF_timeseries_section_%d.npy', sim_file=vc_parser.default_sim_file, n_t0=10000, fnum=0, output_dir='napa_EWT_m%s_time_figs' % magstr, do_local_fit=False)
+	output_dir='napa_EWT_m%s_time_figs' % magstr
+	b= vc_parser.EMC_EWT_figs(section_ids=napa_sections, m0=this_m, fits_data_file_CDF='%s/VC_CDF_Weibull_fits_dump.npy' % weibull_recurr_output_dir, WT_catalog_format='data/VC_CFF_timeseries_section_%d.npy', sim_file=vc_parser.default_sim_file, n_t0=10000, fnum=0, output_dir=output_dir, do_local_fit=False)
 	#
 	Z=simple_mpp_optimizer(sections='napa', section_names=None, start_year=10000., m0=this_m, b_min=-.1, b_max=.1, d_b=.01, nyquist_min=.2, nyquist_max=.8, d_nyquist=.01,  nits=1000, keep_set=False, dump_file='dumps/napa_m%d_mpp_roc_optimize.npy' % this_m, n_cpus=None)
 	

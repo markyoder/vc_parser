d4eac4b89eea41d09f2387d4cdc4b9b5b4112dd2
MarkYoder <mark.yoder@gmail.com>
2015-02-02 20:32:10 -0800

    adding some runner functionality...

--- runner_roc_lt_figs.py
+++ runner_roc_lt_figs.py
@@ -5,7 +5,32 @@ import matplotlib
 matplotlib.use('Agg')
 import pylab as plt
 
+import multiprocessing as mpp
+
 import vc_paper_emc_figs as vpp
 
-a=vpp.create_ROC_figs_LT_data(section_ids = vpp.vc_parser.emc_sections, nits=2500, num_roc_points=100)
 
+def f1():
+	a=vpp.create_ROC_figs_LT_data(section_ids = vpp.vc_parser.emc_sections, nits=2500, num_roc_points=100)
+
+def f2():
+	a=vpp.create_ROC_figs_data(section_ids = vpp.vc_parser.emc_sections, nits=2500, num_roc_points=100)
+
+if __name__ == '__main__':
+	#print "executing..."
+	#
+	jobs = []
+	
+	p1 = mpp.Process(target=f1)
+	jobs.append(p)
+	p1.start()
+	#
+	p2 = mpp.Process(target=f2)
+	jobs.append(p2)
+	p2.start()
+	#
+	print "jobs started."
+	p1.join()
+	p2.join()
+	#
+	
